545c2989ec375de732a93dcadbbc095cf551c533
Vedant Phatangare <106079739+VedantPhatangare@users.noreply.github.com>
2024-08-08 09:02:25 +0000

    data tyes covered

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,16 @@
+let Roll = 14 //number
+// null
+let Name = "Vedant" //string
+//symbol
+let pass= true //boolean(true/false)
+// BigInt
+let a; //undefined
+
+// console.log(typeof(Roll))
+// console.log(typeof null)
+// console.log(typeof "Vedant")
+// console.log(typeof Symbol)
+// console.log(typeof pass)
+// console.log(typeof BigInt)
+// console.log(typeof a)
+console.table([typeof(Roll),typeof null,typeof "Vedant", typeof Symbol, typeof pass,typeof BigInt,typeof a])
