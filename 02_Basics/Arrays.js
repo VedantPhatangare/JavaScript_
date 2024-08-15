c87eacfc5a2f373378ecbbb27c7433f373449d97
vedantcomp <106079739+vedantcomp@users.noreply.github.com>
2024-08-15 17:04:40 +0530

    Arrays Done!

---
+++ 02_Basics/Arrays.js
@@ -0,0 +1,9 @@
+// [] brackets, {} Braces, () Parenthesis
+//arrays in js are resizable and mixed data types, cannot acces like arr["ved"]
+//push,pop (for insert and remove from end)
+//unshift(add at start),shit(remove firststarting element) but takes time bcz all index need to shift and unshift
+//slice  vs splice 
+//slice arr.slice(0,3) -> gives values from arr[0],1,2, dosent modifies the original array
+//splice arr.splice(0,3) -> returns and removes values arr[0],1,2,arr[3] from original array 
+
+//includes, indexOf, join (return a string with all values of array separated by commas)
---
+++ 02_Basics/Arrays2.js
@@ -0,0 +1,37 @@
+let arr = [1,2,3]
+let arr2 = [3,4,5]
+//push, concat, spread-operztor
+// arr.push(arr2) //modifies original array but inserts array inside array instead of combining them
+// console.log(arr)
+// let arr3 = arr.concat(arr2) //it actually combines two arrays and return a new array, dosent modifies the original array
+// console.log(arr3)
+// let arr3 = [...arr,...arr2] //... is a spread operator which dissloves the array contsiner but preserves its elements
+// console.log(arr3)
+
+
+// // Array.flat
+// let complexArr = [6,7,[1,2,3],8, [9,[10,11]]]
+// let flattendArr = complexArr.flat(2) //infinity as depth also can be given
+// console.log(flattendArr)
+
+//Array.isArray
+// console.log(Array.isArray("Ved"))
+
+
+//Array.from
+// console.log(Array.from("Vedant")) // different from split as it considers spaces also
+// let map = [[1,2],[2,3],[2,3]]
+// console.log(Array.from(map))
+// const mapper = new Map([ //this way keys are the actual first element of every pair
+//         ["1", "a"],
+//         ["2", "b"],
+//     ]);
+// let mapper = [["1","a"],["2","b"]] //This way keys are default 0 ,1... 
+//   console.log(Array.from(mapper.values()))
+//   console.log(Array.from(mapper.keys()))
+
+//Array.of
+// let a = 100
+// let b = 200
+// let c = 300
+// console.log(Array.of(a,b,c))
