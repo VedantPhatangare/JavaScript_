e06d36b19861b30540f6862d723f69e5c0581a11
vedantcomp <106079739+vedantcomp@users.noreply.github.com>
2024-08-19 17:36:15 +0530

    ControlFlow_operatorsDone!

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,2 @@
+// if(2!=="2") console.log("Data Types are different"), console.log("This type of syntax is not recommended") 
+    // implicit scope
---
+++ 04_ControlFlow/two.js
@@ -0,0 +1,50 @@
+// flasy values
+// false,0,-0,BigInt 0n, "",null,undefined,NaN
+// truthy values
+// function(){}, {}(object), [], " ", 'false',"0"
+
+
+// if array is empty
+/*
+let emptyArray = []
+
+if(emptyArray.length === 0){
+    console.log("Array is Empty")
+}
+*/
+
+
+
+
+// if object is emptyy
+/*
+const emptyObject = {}
+
+if(emptyObject.keys().length === 0){
+    console.log("Object is empty")
+}
+*/
+
+
+
+/*
+//null coilescense operator
+//works on null or un defined only
+let var1;
+// var1 = null ?? 4;
+// var1 = undefined ?? 4;
+// var1 = undefined ?? 4 ?? 10;
+// var1 = undefined ?? null ?? 10;
+// console.log(var1)
+*/
+
+
+
+// Terniary Operator
+// condition? true: false
+
+const emtyarr = []
+emtyarr == false? console.log("Falsy Value") : console.log("Truthy Value")
+
+
+
